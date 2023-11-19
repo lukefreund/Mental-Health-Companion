8e1fd8de9ca3aadcd5f2650cf3ba4eb5217cd61b
liamdichiro <liam.dichiro@mail.mcgill.ca>
2023-11-19 04:27:23 -0500

    Clean up

--- client/src/pages/calendar/Calendar.js
+++ client/src/pages/calendar/Calendar.js
@@ -1,9 +1,11 @@
 import React from "react";
 import EmotionCalendar from "../../components/emotionCalendar/emotionCalendar.js";
+import Navbar from "../../components/navbar/Navbar";
 
 const Calendar = () => {
   return (
     <div className="calendarPage">
+      <Navbar />
       {/* You can add a header or other components here */}
       <EmotionCalendar />
       {/* Any additional components can go here */}
