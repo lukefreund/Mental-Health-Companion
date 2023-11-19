a1a4c8f97643237fad50efa08664cee5482efdce
lukefreund <luke.freund@mail.mcgill.ca>
2023-11-19 05:58:10 -0500

    added buttons

--- client/src/components/emotionCalendar/emotionCalendar.js
+++ client/src/components/emotionCalendar/emotionCalendar.js
@@ -4,13 +4,59 @@ import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import './emotionCalendar.css'; // Your custom CSS
 import EditJournalEntryModal from '../editJournalEntryModal/editJournalEntryModal';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const EmotionCalendar = () => {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [entryDetails, setEntryDetails] = useState('Select a date to see journal details.');
   const [showEditModal, setShowEditModal] = useState(false);
   const [currentEntry, setCurrentEntry] = useState({});
+  const [loading, setLoading] = useState(false); // New state for loading status
+  const navigate = useNavigate(); // Hook to navigate to different routes
   const userId = localStorage.getItem('userId');
+
+  const handleDeleteEntry = async (entryId) => {
+    if (!entryId) {
+      alert('No entry selected for deletion.');
+      return;
+    }
+  
+    try {
+      const response = await axios.delete(`http://localhost:8080/journal/${entryId}`);
+      console.log(response.data);
+      alert('Journal entry deleted successfully.');
+      
+      // Update the local state to reflect the deletion
+      setEntryDetails('Select a date to see journal details.');
+      setCurrentEntry({});
+      fetchJournalEntry(selectedDate); // Refresh entries after deletion
+    } catch (error) {
+      console.error('Error deleting journal entry:', error);
+      alert('Failed to delete the journal entry.');
+    }
+  };
+
+  const handleAnalyzeClick = async () => {
+    try {
+      if (!currentEntry.entry.trim() || !currentEntry.emotion.trim()) {
+        alert("Please ensure both entry and emotion are filled out before analysis.");
+        return; // Exit the function if either is empty
+      }
+
+      setLoading(true); // Start loading
+      const response = await axios.post('http://localhost:8080/analyze', {
+        entry: currentEntry.entry,
+        emotion: currentEntry.emotion
+      });
+      navigate('/analysis-result', { state: { analysisResult: response.data } });
+    } catch (error) {
+      console.error('Error analyzing journal entry:', error);
+      // Handle error, e.g., show error message to user
+    } finally {
+      setLoading(false); // Stop loading
+    }
+  };
 
   const handleEditClick = (entry) => {
     setCurrentEntry(entry);
@@ -92,7 +138,14 @@ const EmotionCalendar = () => {
         <div className="journalDetails">
           <h3>Selected Date: {selectedDate.toDateString()}</h3>
           <p>{entryDetails}</p>
-          <button className="editButton" onClick={() => handleEditClick(currentEntry)}>Edit Entry</button>
+          <div className="actionButtons">
+            <button className="editButton" onClick={() => handleEditClick(currentEntry)}>Edit Entry</button>
+            <button className="deleteButton" onClick={() => handleDeleteEntry(currentEntry._id)}>Delete Entry</button>
+            <button className="analyzeButton" onClick={handleAnalyzeClick} disabled={loading}>
+              {loading ? 'Analyzing...' : 'Analyze Entry'}
+            </button>
+          </div>
+          {loading && <div className="loader">Loading...</div>} {/* Loader Element */}
         </div>
         <EditJournalEntryModal
         isOpen={showEditModal}
